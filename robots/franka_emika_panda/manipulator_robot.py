6f34dd5015a1f2c712069f6776992f4b3ceb48df
zahirmahammad <zahirmd@umd.edu>
2025-09-07 00:35:06 -0400

    latest

--- robots/franka_emika_panda/manipulator_robot.py
+++ robots/franka_emika_panda/manipulator_robot.py
@@ -4,10 +4,24 @@ import mujoco_viewer
 import numpy as np
 import os
 from mujoco import MjModel
+from target import Target
+from dm_control import mjcf
+
 
 # current directory
 path = "robots\\franka_emika_panda\\scenes\\scene.xml"
-model = mujoco.MjModel.from_xml_path(path)
+# model = mujoco.MjModel.from_xml_path(path)
+
+robot = mjcf.from_path(path)            # Load the robot model
+mjcf_model = mjcf.RootElement()         # Create a new MJCF model
+mjcf_model.attach(robot)                # Attach the robot model to the MJCF model
+target = Target(mjcf_model)             # Add a target to the MJCF model
+
+physics = mjcf.Physics.from_mjcf_model(mjcf_model)      # Create a physics object from the MJCF model
+model = physics.model.ptr                # Get the model pointer from the physics object
+
+
+
 
 
 # Load the robot model
@@ -18,6 +32,10 @@ print(f"Input shape: {data.ctrl.shape}")
 joint_positions = np.zeros(data.ctrl.shape[0], dtype=np.float64)
 # --------------------------------------------------
 
+
+
+# Simulate and visualize the robot
+# --------------------------------------------------
 # Create a viewer for visualization
 viewer = mujoco_viewer.MujocoViewer(model, data)
 
@@ -30,6 +48,8 @@ while True:
     #         print(joint_positions)
     data.ctrl[:] = joint_positions  # Update the robot's joint positions
     joint_positions[0] = 1.57
+    joint_positions[1] = 1.57
+    print(f"Current joint positions: {data.ctrl}")
     mujoco.mj_step(model, data)
     viewer.render()  # Render the simulation (visualization)
 
@@ -39,4 +59,3 @@ while True:
 
 viewer.close()
 # --------------------------------------------------
-
